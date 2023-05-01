b97f4e16a6edd4db01107ba67b736710035edeaf
Faiz Essa <faiz.essa@gmail.com>
2023-05-01 19:28:02 -0400

    Cleaned up event study plots, wrote code for sun & ab event studies, made latex table w/ point estimates

--- .Rproj.user/BC2EE215/sources/per/t/8F05EC0A-contents
+++ .Rproj.user/BC2EE215/sources/per/t/8F05EC0A-contents
@@ -18,24 +18,27 @@ prowess_panel <- read_csv("Data/Prowess/prowess_panel.csv")
 
 #### DATA WRANGLING ####
 gdelt_panel <- gdelt_panel %>%
-  mutate(rel_time = ifelse(group != 0, time-group, NA)) %>%
+  mutate(cohort = ifelse(group == 0, 100, group)) %>%
+  mutate(rel_time = time-cohort) %>%
   mutate(rel_time = ifelse(rel_time <= -8, -8, rel_time),
          rel_time = ifelse(rel_time >=8, 8, rel_time)) %>%
-  mutate(cohort = ifelse(group == 0, 100, group)) 
-  
+  rename(District = ActionGeo_ADM2Code, Time = time)
+
 gdelt_tone_panel <- gdelt_tone_panel %>%
   mutate(cohort = ifelse(group == 0, 100, group)) %>%
   mutate(rel_time = time-cohort) %>%
   mutate(rel_time = ifelse(rel_time <= -8, -8, rel_time),
          rel_time = ifelse(rel_time >=8, 8, rel_time)) %>%
-  mutate(zscore = scale(AvgGovTone)) 
+  mutate(zscore = scale(AvgGovTone)) %>% 
+  rename(District = Actor1Geo_ADM2Code, Time = time)
 
-prowess_panel <- prowess_panel %>%
+prowess_panel <- prowess_panel%>%
   mutate(cohort = ifelse(group == 0, 100, group)) %>%
   mutate(rel_time = time-cohort) %>%
   mutate(rel_time = ifelse(rel_time <= -8, -8, rel_time),
-         rel_time = ifelse(rel_time >=8, 8, rel_time)) 
-  
+         rel_time = ifelse(rel_time >=8, 8, rel_time)) %>%
+  rename(District = Actor1Geo_ADM2Code, Time = time)
+
 #### GDELT EVENT REGRESSIONS ####
 
 # list of dependent variables
@@ -46,11 +49,11 @@ gdelt_depvars <- c("assault_count", "fight_count", "protest_count",
 for (y in gdelt_depvars){
   
   formula <- as.formula(
-    paste(y, "~sunab(cohort, rel_time, ref.p=-4) | ActionGeo_ADM2Code + time", 
+    paste(y, "~sunab(cohort, rel_time, ref.p=-4) | District + Time", 
           sep = ""))
   
   model <- feols(formula,
-                 cluster = ~ActionGeo_ADM2Code,
+                 cluster = ~District,
                  data = gdelt_panel)
   
   assign(y, model)
@@ -81,15 +84,15 @@ dev.off()
 #### GDELT TONE REGRESSION ####
 
 zscore <- feols(zscore ~ sunab(cohort, rel_time, ref.p=-4) |
-                  Actor1Geo_ADM2Code + time,
-                cluster = ~Actor1Geo_ADM2Code,
+                  District + Time,
+                cluster = ~District,
                 data = gdelt_tone_panel)
 
 #### GDELT TONE PLOT ####
 
 pdf("results/draft2sunab/gdelt_tone_sunab.pdf")
 iplot(zscore, sep = 0.2, ref.line = -4,
-      xlab = 'Weeks to shutdown',
+      xlab = 'Weeks to first shutdown',
       main = '')
 legend("bottomright", col = 1, pch = 20, 
        legend =  c("Standardized Avg. Tone"))
@@ -103,17 +106,17 @@ for (y in prowess_depvars) {
   log <- paste("log(",y,")")
   
   formula <- as.formula(
-    paste(y, "~sunab(cohort, rel_time, ref.p=-4) | regddname + time", 
+    paste(y, "~sunab(cohort, rel_time, ref.p=-4) | District + Time", 
           sep = ""))
   log_formula <- as.formula(
-    paste(log, "~i(sunab(cohort, rel_time, ref.p=-4) | regddname + time", 
+    paste(log, "~i(sunab(cohort, rel_time, ref.p=-4) | District + Time", 
           sep = ""))
   
   model <- feols(formula,
-                 cluster = ~regddname,
+                 cluster = ~District,
                  data = prowess_panel)
   log_model <- feols(log_formula,
-                     cluster = ~regddname,
+                     cluster = ~District,
                      data = prowess_panel)
   
   log_model_name <- paste("log", y, sep = "_")
@@ -138,6 +141,31 @@ iplot(list(log_AvgBSEPrice, log_AvgNSEPrice), sep = 0.25, ref.line = -4,
 legend("bottomleft", col = c(1, 2), pch = c(20, 17), 
        legend =  c("ln(Avg. BSE Price)", "ln(Avg. NSE Price)"))
 dev.off()
+
+#### LATEX TABLE ####
+test <- etable(assault_count, fight_count, protest_count, 
+               log_assaults, log_fights, log_protests,
+               zscore, 
+               AvgBSEPrice, AvgNSEPrice,
+               log_AvgBSEPrice, log_AvgNSEPrice,
+               dict = c(assault_count = "Assaults",
+                        fight_count = "Fights",
+                        protest_count = "Protests",
+                        log_assaults = "ln(Assaults+1)",
+                        log_fights = "ln(Fights+1)",
+                        log_protests = "ln(Protests+1)",
+                        zscore = "Tone of Gov. Events",
+                        AvgBSEPrice = "BSE Price",
+                        AvgNSEPrice = "NSE Price",
+                        log(AvgBSEPrice) = "ln(BSE Price)",
+                        log(AvgNSEPrice) = "ln(NSE Price)",
+                        time_from_sd = ""),
+               i.equal = "",
+               label = "tab:sunab",
+               title = "\cite{sun2021estimating} event study estimates",
+               file = "results/draft2sunab/sunab_estimates.tex",
+               tex = TRUE)
+
 
 
 
--- Data Analysis Code/Draft2EventStudiesApr29.R
+++ Data Analysis Code/Draft2EventStudiesApr29.R
@@ -1,6 +1,6 @@
 # Faiz Essa
 # Event Studies April 29th
-# April 29th, 2023
+# May 1, 2023
 
 #### SETUP ####
 
@@ -10,6 +10,7 @@ rm(list=ls())
 library(tidyverse)
 library(fixest)
 library(readr)
+library(stargazer)
 
 # data import 
 gdelt_panel <- read_csv("Data/GDELT/gdelt_panel.csv")
@@ -22,7 +23,8 @@ gdelt_panel <- gdelt_panel %>%
   mutate(time_from_sd = ifelse(group == 0, 0, time-group)) %>%
   # set >= 8 to 8, set <= -8 to -8 
   mutate(time_from_sd = ifelse(time_from_sd <= -8, -8, time_from_sd),
-         time_from_sd = ifelse(time_from_sd >=8, 8, time_from_sd))
+         time_from_sd = ifelse(time_from_sd >=8, 8, time_from_sd)) %>%
+  rename(District = ActionGeo_ADM2Code, Time = time)
 
 gdelt_tone_panel <- gdelt_tone_panel %>%
   # time to first shutdown
@@ -30,14 +32,16 @@ gdelt_tone_panel <- gdelt_tone_panel %>%
   # set >= 8 to 8, set <= -8 to -8 
   mutate(time_from_sd = ifelse(time_from_sd <= -8, -8, time_from_sd),
          time_from_sd = ifelse(time_from_sd >=8, 8, time_from_sd)) %>%
-  mutate(zscore = scale(AvgGovTone)) 
+  mutate(zscore = scale(AvgGovTone)) %>%
+  rename(District = Actor1Geo_ADM2Code, Time = time)
 
 prowess_panel <- prowess_panel %>%
   # time to first shutdown
   mutate(time_from_sd = ifelse(group == 0, 0, time-group)) %>%
   # set >= 8 to 8, set <= -8 to -8 
   mutate(time_from_sd = ifelse(time_from_sd <= -8, -8, time_from_sd),
-         time_from_sd = ifelse(time_from_sd >=8, 8, time_from_sd))
+         time_from_sd = ifelse(time_from_sd >=8, 8, time_from_sd)) %>%
+  rename(District = regddname, Time = time)
 
 #### GDELT EVENT REGRESSIONS ####
 
@@ -49,11 +53,11 @@ gdelt_depvars <- c("assault_count", "fight_count", "protest_count",
 for (y in gdelt_depvars){
   
   formula <- as.formula(
-    paste(y, "~i(time_from_sd,ref=-4) | ActionGeo_ADM2Code + time", 
+    paste(y, "~i(time_from_sd,ref=-4) | District + Time", 
           sep = ""))
   
   model <- feols(formula,
-                 cluster = ~ActionGeo_ADM2Code,
+                 cluster = ~District,
                  data = gdelt_panel)
   
   assign(y, model)
@@ -84,14 +88,14 @@ dev.off()
 #### GDELT TONE REGRESSION ####
 
 zscore <- feols(zscore ~ i(time_from_sd, ref = -4) |
-                  Actor1Geo_ADM2Code + time,
-                cluster = ~Actor1Geo_ADM2Code,
+                  District + Time,
+                cluster = ~District,
                 data = gdelt_tone_panel)
 
 #### GDELT TONE PLOT ####
 pdf("results/Draft2EventStudiesV2/gdelt_tone.pdf")
 iplot(zscore, sep = 0.2, ref.line = -4,
-      xlab = 'Weeks to shutdown',
+      xlab = 'Weeks to first shutdown',
       main = '')
 legend("bottomright", col = 1, pch = 20, 
        legend =  c("Standardized Avg. Tone"))
@@ -105,17 +109,17 @@ for (y in prowess_depvars) {
   log <- paste("log(",y,")")
   
   formula <- as.formula(
-    paste(y, "~i(time_from_sd,ref=-4) | regddname + time", 
+    paste(y, "~i(time_from_sd,ref=-4) | District + Time", 
           sep = ""))
   log_formula <- as.formula(
-    paste(log, "~i(time_from_sd,ref=-4) | regddname + time", 
+    paste(log, "~i(time_from_sd,ref=-4) | District + Time", 
           sep = ""))
   
   model <- feols(formula,
-                 cluster = ~regddname,
+                 cluster = ~District,
                  data = prowess_panel)
   log_model <- feols(log_formula,
-                     cluster = ~regddname,
+                     cluster = ~District,
                      data = prowess_panel)
   
   log_model_name <- paste("log", y, sep = "_")
@@ -141,4 +145,26 @@ legend("bottomleft", col = c(1, 2), pch = c(20, 17),
        legend =  c("ln(Avg. BSE Price)", "ln(Avg. NSE Price)"))
 dev.off()
 
-
+#### LATEX TABLE ####
+test <- etable(assault_count, fight_count, protest_count, 
+               log_assaults, log_fights, log_protests,
+               zscore, 
+               AvgBSEPrice, AvgNSEPrice,
+               log_AvgBSEPrice, log_AvgNSEPrice,
+               dict = c(assault_count = "Assaults",
+                        fight_count = "Fights",
+                        protest_count = "Protests",
+                        log_assaults = "ln(Assaults+1)",
+                        log_fights = "ln(Fights+1)",
+                        log_protests = "ln(Protests+1)",
+                        zscore = "Tone of Gov. Events",
+                        AvgBSEPrice = "BSE Price",
+                        AvgNSEPrice = "NSE Price",
+                        log(AvgBSEPrice) = "ln(BSE Price)",
+                        log(AvgNSEPrice) = "ln(NSE Price)",
+                        time_from_sd = ""),
+               i.equal = "",
+               label = "tab:twfe",
+               title = "Two-way fixed effects event study estimates",
+               file = "results/Draft2EventStudiesV2/twfe_estimates.tex",
+               tex = TRUE)
